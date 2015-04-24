d2db31c6ba83597252df1fc10f02895de3c344bd
christian <christian@Jarvis>
2015-04-24 21:12:02 +0200

    Ultima clase modificada

---
+++ src/PaqueteChristian/ModificadaPorAmbos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteChristian;
+
+/**
+ *
+ * @author christian
+ */
+public class ModificadaPorAmbos {
+    public void ClaseChristian(){
+        System.out.println("Hasta luego Mundo");
+    }
+}
