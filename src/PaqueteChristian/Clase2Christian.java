c8e9df9c42158daae5752039ba8dd5a39a32caad
christian <christian@Jarvis>
2015-04-24 21:06:02 +0200

    Commit de clase nueva

---
+++ src/PaqueteChristian/Clase2Christian.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteChristian;
+
+/**
+ *
+ * @author christian
+ */
+public class Clase2Christian {
+    public void ClaseChristian(){
+        System.out.println("Hola de nuevo Mundo");
+    }
+    
+}
